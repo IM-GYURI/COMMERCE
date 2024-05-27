acaaddad6810007d763ae0f048721e5afff647a4
IM-GYURI <kgulr0517@gmail.com>
2024-05-27 21:52:35 +0900

    feat : add Seller Entity
    id, sellerKey, email, password, name, phone, address, role

---
+++ src/main/java/com/zerobase/commerce/entity/SellerEntity.java
@@ -0,0 +1,105 @@
+package com.zerobase.commerce.entity;
+
+import static com.zerobase.commerce.type.Role.SELLER;
+
+import com.zerobase.commerce.type.Role;
+import com.zerobase.commerce.util.RoleTypeConverter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.Collections;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * 판매자 엔티티 : 아이디, 판매자 키, 이메일, 비밀번호, 이름, 전화번호, 주소, 역할
+ */
+@Getter
+@NoArgsConstructor
+@Table(indexes = {@Index(columnList = "seller_key")})
+@Entity(name = "Seller")
+public class SellerEntity extends BaseEntity implements UserDetails {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true, name = "seller_key")
+  private String sellerKey;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false, unique = true)
+  private String phone;
+
+  @Column(nullable = false)
+  private String address;
+
+  @Convert(converter = RoleTypeConverter.class)
+  @Column(nullable = false)
+  private Role role;
+
+  @Builder
+  public SellerEntity(String sellerKey, String email, String password, String name, String phone,
+      String address) {
+    this.sellerKey = sellerKey;
+    this.email = email;
+    this.password = password;
+    this.name = name;
+    this.phone = phone;
+    this.address = address;
+    this.role = SELLER;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Collections.singletonList(new SimpleGrantedAuthority(role.getKey()));
+  }
+
+  @Override
+  public String getPassword() {
+    return null;
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
