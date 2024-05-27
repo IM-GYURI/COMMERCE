9fffb4a16e587d67b625369c0cc12d5137598e77
IM-GYURI <kgulr0517@gmail.com>
2024-05-27 21:40:31 +0900

    feat : add Customer Entity
    id, customerKey, email, password, name, phone, address, birth, point, role

---
+++ src/main/java/com/zerobase/commerce/entity/CustomerEntity.java
@@ -0,0 +1,114 @@
+package com.zerobase.commerce.entity;
+
+import static com.zerobase.commerce.type.Role.CUSTOMER;
+
+import com.zerobase.commerce.type.Role;
+import com.zerobase.commerce.util.RoleTypeConverter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.Collections;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * 고객 엔티티 : 아이디, 고객 키, 이메일, 비밀번호, 이름, 전화번호, 주소, 생일, 포인트, 역할
+ */
+@Getter
+@NoArgsConstructor
+@Table(indexes = {@Index(columnList = "customer_key")})
+@Entity(name = "Customer")
+public class CustomerEntity extends BaseEntity implements UserDetails {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true, name = "customer_key")
+  private String customerKey;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false, unique = true)
+  private String phone;
+
+  @Column(nullable = false)
+  private String address;
+
+  @Column(nullable = false)
+  private String birth;
+
+  @ColumnDefault("0")
+  private Long point;
+
+  @Convert(converter = RoleTypeConverter.class)
+  @Column(nullable = false)
+  private Role role;
+
+  @Builder
+  public CustomerEntity(String customerKey, String email, String password, String name,
+      String phone, String address, String birth, Long point) {
+    this.customerKey = customerKey;
+    this.email = email;
+    this.password = password;
+    this.name = name;
+    this.phone = phone;
+    this.address = address;
+    this.birth = birth;
+    this.point = point;
+    this.role = CUSTOMER;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Collections.singletonList(new SimpleGrantedAuthority(role.getKey()));
+  }
+
+  @Override
+  public String getPassword() {
+    return null;
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
