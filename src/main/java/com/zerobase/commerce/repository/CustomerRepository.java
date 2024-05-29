1ea40ace693c70f207866005f7c949926793912f
IM-GYURI <kgulr0517@gmail.com>
2024-05-30 05:50:00 +0900

    feat : add existsByEmail and findByEmail
    use Optional

--- src/main/java/com/zerobase/commerce/repository/CustomerRepository.java
+++ src/main/java/com/zerobase/commerce/repository/CustomerRepository.java
@@ -1,8 +1,13 @@
 package com.zerobase.commerce.repository;
 
 import com.zerobase.commerce.entity.CustomerEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
+
+  boolean existsByEmail(String email);
+
+  Optional<CustomerEntity> findByEmail(String email);
 
 }
