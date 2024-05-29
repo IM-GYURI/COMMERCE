88b0e8f2ac008fdf44256a8776dacc025c95b2c6
IM-GYURI <kgulr0517@gmail.com>
2024-05-30 05:53:40 +0900

    feat : add Customer Sign In Dto
    Information of Customer for Sign In

---
+++ src/main/java/com/zerobase/commerce/dto/customer/CustomerSignInDto.java
@@ -0,0 +1,21 @@
+package com.zerobase.commerce.dto.customer;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ * 로그인 Dto
+ *
+ * @param email
+ * @param password
+ */
+public record CustomerSignInDto(
+    @NotBlank
+    @Email
+    String email,
+
+    @NotBlank
+    String password
+) {
+
+}
