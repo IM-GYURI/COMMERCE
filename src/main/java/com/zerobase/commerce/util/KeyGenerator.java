2285846faa6992a954328f1105d77424112050c2
IM-GYURI <kgulr0517@gmail.com>
2024-05-30 05:51:59 +0900

    feat : add Key Generator
    create UUID

---
+++ src/main/java/com/zerobase/commerce/util/KeyGenerator.java
@@ -0,0 +1,16 @@
+package com.zerobase.commerce.util;
+
+import java.util.UUID;
+import org.springframework.stereotype.Component;
+
+/**
+ * 인조 식별자 생성을 위한 UUID 생성 객체
+ */
+
+@Component
+public class KeyGenerator {
+
+  public static String generateKey() {
+    return UUID.randomUUID().toString().replace("-", "");
+  }
+}
