56a4a52638b136457539e29a70e934557b8ddac1
IM-GYURI <kgulr0517@gmail.com>
2024-05-31 05:34:21 +0900

    feat : add validation annotations
    validation for email, password

--- customer-api/src/main/java/zerobase/customerapi/dto/customer/CustomerSignInDto.java
+++ customer-api/src/main/java/zerobase/customerapi/dto/customer/CustomerSignInDto.java
@@ -2,6 +2,7 @@ package zerobase.customerapi.dto.customer;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 /**
  * 로그인 Dto
@@ -10,11 +11,13 @@ import jakarta.validation.constraints.NotBlank;
  * @param password
  */
 public record CustomerSignInDto(
-    @NotBlank
-    @Email
+    @NotBlank(message = "이메일은 필수 입력 값 입니다.")
+    @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
+    @Email(message = "이메일 형식에 맞지 않습니다.")
     String email,
 
-    @NotBlank
+    @NotBlank(message = "비밀번호는 필수 입력 값 입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     String password
 ) {
 
