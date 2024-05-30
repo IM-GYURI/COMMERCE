103db95e2930265950aaf155a45c8ada7ce28c4c
IM-GYURI <kgulr0517@gmail.com>
2024-05-31 05:18:43 +0900

    refactor : CustomUserDetailsService of seller api
    change repository from CustomerRepository to SellerRepository

---
+++ seller-api/src/main/java/zerobase/sellerapi/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package zerobase.sellerapi.service;
+
+import static zerobase.sellerapi.exception.ErrorCode.SELLER_NOT_FOUND;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import zerobase.sellerapi.repository.SellerRepository;
+
+/**
+ * spring security의 UserDetailsService 구현체
+ */
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+  private final SellerRepository sellerRepository;
+
+  /**
+   * authenticate 메소드가 실행되면, 해당 메소드를 호출하여 유저 검증 후 인증된 유저 객체를 리턴
+   */
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    return sellerRepository.findByEmail(email)
+        .orElseThrow(() -> new UsernameNotFoundException(
+            SELLER_NOT_FOUND.getMessage()
+        ));
+  }
+}
