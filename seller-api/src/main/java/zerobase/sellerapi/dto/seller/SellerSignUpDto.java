f3b81630ebd60e0e9383744baaa2681398334e4e
IM-GYURI <kgulr0517@gmail.com>
2024-05-31 05:35:48 +0900

    feat : add validation annotations
    validation for email, password

--- seller-api/src/main/java/zerobase/sellerapi/dto/seller/SellerSignUpDto.java
+++ seller-api/src/main/java/zerobase/sellerapi/dto/seller/SellerSignUpDto.java
@@ -1,6 +1,8 @@
 package zerobase.sellerapi.dto.seller;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,10 +18,13 @@ import zerobase.sellerapi.entity.SellerEntity;
 @AllArgsConstructor
 public class SellerSignUpDto {
 
-  @NotBlank
+  @NotBlank(message = "이메일은 필수 입력 값 입니다.")
+  @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
+  @Email(message = "이메일 형식에 맞지 않습니다.")
   private String email;
 
-  @NotBlank
+  @NotBlank(message = "비밀번호는 필수 입력 값 입니다.")
+  @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
   private String password;
 
   @NotBlank
