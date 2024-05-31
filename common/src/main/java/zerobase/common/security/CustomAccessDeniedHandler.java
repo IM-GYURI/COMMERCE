f744566a859a7ed80f4198d771a9d4c465e2c1df
IM-GYURI <kgulr0517@gmail.com>
2024-05-31 20:20:59 +0900

    refactore : move CustomAccessDeniedHandler to common module

---
+++ common/src/main/java/zerobase/common/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,19 @@
+package zerobase.common.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+  @Override
+  public void handle(HttpServletRequest request,
+      HttpServletResponse response,
+      AccessDeniedException accessDeniedException)
+      throws IOException, ServletException {
+    response.sendError(HttpServletResponse.SC_FORBIDDEN, "접근 권한이 없습니다.");
+  }
+}
