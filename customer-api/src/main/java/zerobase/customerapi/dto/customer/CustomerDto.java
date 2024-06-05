f6384705bb805c320bfd64e76d8d01303da830b3
IM-GYURI <kgulr0517@gmail.com>
2024-06-06 05:54:50 +0900

    feat : add properties
    phone, address, point, birth, createdAt, modifiedAt

--- customer-api/src/main/java/zerobase/customerapi/dto/customer/CustomerDto.java
+++ customer-api/src/main/java/zerobase/customerapi/dto/customer/CustomerDto.java
@@ -1,5 +1,7 @@
 package zerobase.customerapi.dto.customer;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,8 +16,14 @@ import zerobase.customerapi.entity.CustomerEntity;
 public class CustomerDto {
 
   private String customerKey;
-  private String name;
   private String email;
+  private String name;
+  private String phone;
+  private String address;
+  private Long point;
+  private LocalDate birth;
+  private LocalDateTime createdAt;
+  private LocalDateTime modifiedAt;
   private Role role;
 
   /**
@@ -24,8 +32,14 @@ public class CustomerDto {
   public static CustomerDto fromEntity(CustomerEntity customer) {
     return CustomerDto.builder()
         .customerKey(customer.getCustomerKey())
-        .name(customer.getName())
         .email(customer.getEmail())
+        .name(customer.getName())
+        .phone(customer.getPhone())
+        .address(customer.getAddress())
+        .point(customer.getPoint())
+        .birth(customer.getBirth())
+        .createdAt(customer.getCreatedAt())
+        .modifiedAt(customer.getModifiedAt())
         .role(customer.getRole())
         .build();
   }
