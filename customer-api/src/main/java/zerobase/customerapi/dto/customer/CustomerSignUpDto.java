9acd0fbe1a53c7a0910f17bba331f248ec6eb34e
IM-GYURI <kgulr0517@gmail.com>
2024-06-06 04:05:35 +0900

    feat : add Pattern of name and phone number
    name Pattern = length of 2 ~ 10
    phone number Patter = 010-0000-0000

--- customer-api/src/main/java/zerobase/customerapi/dto/customer/CustomerSignUpDto.java
+++ customer-api/src/main/java/zerobase/customerapi/dto/customer/CustomerSignUpDto.java
@@ -22,25 +22,27 @@ import zerobase.customerapi.entity.CustomerEntity;
 @AllArgsConstructor
 public class CustomerSignUpDto {
 
-  @NotBlank(message = "이메일은 필수 입력 값 입니다.")
+  @NotBlank(message = "이메일은 필수 입력 값입니다.")
   @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
   @Email(message = "이메일 형식에 맞지 않습니다.")
   private String email;
 
-  @NotBlank(message = "비밀번호는 필수 입력 값 입니다.")
-  @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+  @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+  @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자로, 영문 대소문자, 숫자, 특수문자를 사용하세요.")
   private String password;
 
-  @NotBlank
+  @NotBlank(message = "이름은 필수 입력 값입니다.")
+  @Pattern(regexp = "^.{2,10}$", message = "이름은 2~10자이어야 합니다.")
   private String name;
 
-  @NotBlank
+  @NotBlank(message = "전화번호는 필수 입력 값입니다.")
+  @Pattern(regexp = "^\\d{3}-\\d{4}-\\d{4}$", message = "전화번호는 010-1234-5678 형식으로 입력해주세요.")
   private String phone;
 
-  @NotBlank
+  @NotBlank(message = "주소는 필수 입력 값입니다.")
   private String address;
 
-  @NotNull(message = "생년월일은 필수 입력 값 입니다.")
+  @NotNull(message = "생년월일은 필수 입력 값입니다.")
   @Past(message = "생년월일을 확인해주세요.")
   @DateTimeFormat(pattern = "yyyy-MM-dd")
   private LocalDate birth;
