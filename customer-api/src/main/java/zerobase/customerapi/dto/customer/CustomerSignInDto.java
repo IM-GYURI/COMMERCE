ade7f780c904ec408a4c3fadbcf5e25350adeb2c
IM-GYURI <kgulr0517@gmail.com>
2024-06-06 04:06:32 +0900

    refactor : delete space

--- customer-api/src/main/java/zerobase/customerapi/dto/customer/CustomerSignInDto.java
+++ customer-api/src/main/java/zerobase/customerapi/dto/customer/CustomerSignInDto.java
@@ -11,13 +11,13 @@ import jakarta.validation.constraints.Pattern;
  * @param password
  */
 public record CustomerSignInDto(
-    @NotBlank(message = "이메일은 필수 입력 값 입니다.")
+    @NotBlank(message = "이메일은 필수 입력값 입니다.")
     @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
     @Email(message = "이메일 형식에 맞지 않습니다.")
     String email,
 
-    @NotBlank(message = "비밀번호는 필수 입력 값 입니다.")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @NotBlank(message = "비밀번호는 필수 입력값 입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자로, 영문 대소문자, 숫자, 특수문자를 사용하세요.")
     String password
 ) {
 
