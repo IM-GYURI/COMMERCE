5ed98fde8be7f20af360876b2ef309c6540cbf20
IM-GYURI <kgulr0517@gmail.com>
2024-06-07 03:52:33 +0900

    refactor : CommonErrorCode implements ErrorCode

---
+++ common/src/main/java/zerobase/common/exception/CommonErrorCode.java
@@ -0,0 +1,19 @@
+package zerobase.common.exception;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum CommonErrorCode implements ErrorCode {
+  // Global
+  RESOURCE_NOT_FOUND(NOT_FOUND, "요청한 자원을 찾을 수 없습니다."),
+  INVALID_REQUEST(BAD_REQUEST, "올바르지 않은 요청입니다.");
+
+  private final HttpStatus status;
+  private final String message;
+}
