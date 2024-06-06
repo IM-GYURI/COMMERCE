cd1053cb2626e58e120c7142dfb903221b691257
IM-GYURI <kgulr0517@gmail.com>
2024-06-07 03:48:26 +0900

    style : delete blank

--- customer-api/src/main/java/zerobase/customerapi/repository/CustomerRepository.java
+++ customer-api/src/main/java/zerobase/customerapi/repository/CustomerRepository.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import zerobase.customerapi.entity.CustomerEntity;
 
-
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
 
   boolean existsByEmail(String email);
