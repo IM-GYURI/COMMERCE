c583583543801a646ef3043e733e1b9f3f3d12e3
IM-GYURI <kgulr0517@gmail.com>
2024-06-07 02:43:06 +0900

    feat : add find by productKey

--- seller-api/src/main/java/zerobase/sellerapi/repository/ProductRepository.java
+++ seller-api/src/main/java/zerobase/sellerapi/repository/ProductRepository.java
@@ -1,8 +1,10 @@
 package zerobase.sellerapi.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import zerobase.sellerapi.entity.ProductEntity;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
+  Optional<ProductEntity> findByProductKey(String productKey);
 }
