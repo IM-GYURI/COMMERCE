6fd2c37daf456d0357a360ba7c5d2fb1243e493e
IM-GYURI <kgulr0517@gmail.com>
2024-06-07 02:43:29 +0900

    feat : add edit, findByProductKeyOrThrow, validateAuthorizationAndGetSeller

--- seller-api/src/main/java/zerobase/sellerapi/service/ProductService.java
+++ seller-api/src/main/java/zerobase/sellerapi/service/ProductService.java
@@ -1,14 +1,21 @@
 package zerobase.sellerapi.service;
 
+import static zerobase.common.exception.ErrorCode.INVALID_REQUEST;
+import static zerobase.common.exception.ErrorCode.PRODUCT_NOT_FOUND;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import zerobase.common.exception.CustomException;
 import zerobase.common.util.KeyGenerator;
+import zerobase.sellerapi.dto.product.EditDto;
 import zerobase.sellerapi.dto.product.ProductDto;
 import zerobase.sellerapi.dto.product.RegistrationDto;
 import zerobase.sellerapi.entity.ProductEntity;
 import zerobase.sellerapi.entity.SellerEntity;
 import zerobase.sellerapi.repository.ProductRepository;
+import zerobase.sellerapi.security.TokenProvider;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +24,7 @@ public class ProductService {
 
   private final ProductRepository productRepository;
   private final SellerService sellerService;
+  private final TokenProvider tokenProvider;
 
   /**
    * 매장 등록 : 판매자 키를 통해 판매자 정보를 찾은 후 매장 등록
@@ -29,6 +37,40 @@ public class ProductService {
     ProductEntity product = registrationDto.toEntity(KeyGenerator.generateKey());
     product.setSeller(seller);
     productRepository.save(product);
+
+    return ProductDto.fromEntity(product);
+  }
+
+  @Transactional
+  public ProductDto edit(EditDto editDto) {
+    ProductEntity product = findByProductKeyOrThrow(editDto.getProductKey());
+
+    product.updateProduct(editDto);
+
+    return ProductDto.fromEntity(product);
+  }
+
+  private ProductEntity findByProductKeyOrThrow(String productKey) {
+    return productRepository.findByProductKey(productKey)
+        .orElseThrow(() -> new CustomException(PRODUCT_NOT_FOUND));
+  }
+
+  public ProductDto validateAuthorizationAndGetSeller(String productKey, String token) {
+    if (token != null && token.startsWith("Bearer ")) {
+      token = token.substring(7);
+    }
+
+    Authentication authentication = tokenProvider.getAuthentication(token);
+    String email = authentication.getName();
+
+    ProductEntity product = findByProductKeyOrThrow(productKey);
+    String sellerKeyFromProductKey = product.getSellerEntity().getSellerKey();
+
+    String sellerKeyFromEmail = sellerService.findByEmail(email).getSellerKey();
+
+    if (!sellerKeyFromProductKey.equals(sellerKeyFromEmail)) {
+      throw new CustomException(INVALID_REQUEST);
+    }
 
     return ProductDto.fromEntity(product);
   }
