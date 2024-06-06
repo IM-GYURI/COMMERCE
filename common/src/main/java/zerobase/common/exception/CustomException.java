e8a8adc552fcf5afeea925d4910886342445b7fa
IM-GYURI <kgulr0517@gmail.com>
2024-06-07 03:49:22 +0900

    refactor : change from class to interface

--- common/src/main/java/zerobase/common/exception/CustomException.java
+++ common/src/main/java/zerobase/common/exception/CustomException.java
@@ -1,22 +1,5 @@
 package zerobase.common.exception;
 
-import lombok.Getter;
+public interface CustomException {
 
-@Getter
-public class CustomException extends RuntimeException {
-
-  private final ErrorCode errorCode;
-  private final String message;
-
-  public CustomException(ErrorCode errorCode) {
-    super(errorCode.getMessage());
-    this.errorCode = errorCode;
-    this.message = errorCode.getMessage();
-  }
-
-  public CustomException(ErrorCode errorCode, String message) {
-    super(message);
-    this.errorCode = errorCode;
-    this.message = message;
-  }
 }
