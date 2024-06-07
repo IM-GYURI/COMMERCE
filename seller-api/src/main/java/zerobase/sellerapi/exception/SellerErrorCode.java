6d8dcdc62ee86128165fc1802d80e645c717ff3c
IM-GYURI <kgulr0517@gmail.com>
2024-06-07 19:18:51 +0900

    refactor : move error codes about product

--- seller-api/src/main/java/zerobase/sellerapi/exception/SellerErrorCode.java
+++ seller-api/src/main/java/zerobase/sellerapi/exception/SellerErrorCode.java
@@ -13,11 +13,8 @@ import zerobase.common.exception.ErrorCode;
 public enum SellerErrorCode implements ErrorCode {
   // Seller
   SELLER_ALREADY_EXISTS(BAD_REQUEST, "이미 존재하는 판매자입니다."),
-  SELLER_NOT_FOUND(NOT_FOUND, "판매자를 찾을 수 없습니다."),
+  SELLER_NOT_FOUND(NOT_FOUND, "판매자를 찾을 수 없습니다.");
 
-  // Product
-  PRODUCT_NOT_FOUND(NOT_FOUND, "상품을 찾을 수 없습니다,");
-  
   private final HttpStatus status;
   private final String message;
 }
