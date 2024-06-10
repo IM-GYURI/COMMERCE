c2658396e9348e0db606b5eea6e0fee61d9488e4
IM-GYURI <kgulr0517@gmail.com>
2024-06-10 19:46:28 +0900

    feat : add @ComponentScan
    about common and seller api

--- seller-api/src/main/java/zerobase/sellerapi/SellerApiApplication.java
+++ seller-api/src/main/java/zerobase/sellerapi/SellerApiApplication.java
@@ -2,10 +2,13 @@ package zerobase.sellerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@ComponentScan(basePackages = {"zerobase.common", "zerobase.sellerapi"})
+
 public class SellerApiApplication {
 
   public static void main(String[] args) {
