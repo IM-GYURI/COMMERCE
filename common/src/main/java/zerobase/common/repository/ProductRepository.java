fbbc8c0271c0bb3bc293ae771087b60347ea5a13
IM-GYURI <kgulr0517@gmail.com>
2024-06-10 23:26:14 +0900

    refactor : move classes related to 'Product'

--- common/src/main/java/zerobase/common/repository/ProductRepository.java
+++ common/src/main/java/zerobase/common/repository/ProductRepository.java
@@ -1,8 +1,8 @@
-package zerobase.sellerapi.repository;
+package zerobase.common.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import zerobase.sellerapi.entity.ProductEntity;
+import zerobase.common.entity.ProductEntity;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
