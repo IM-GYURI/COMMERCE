f1d07cb4bde96b19a758597c1a6636674ae3b0fe
IM-GYURI <kgulr0517@gmail.com>
2024-06-10 23:27:45 +0900

    refactor : move classes related to 'Product' to common

---
+++ common/src/main/java/zerobase/common/dto/product/RegistrationDto.java
@@ -0,0 +1,51 @@
+package zerobase.common.dto.product;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import zerobase.common.entity.ProductEntity;
+import zerobase.common.type.Category;
+import zerobase.common.util.ValidEnum;
+
+/**
+ * 상품 등록용 Dto
+ */
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationDto {
+
+  @NotBlank
+  private String sellerKey;
+
+  @NotBlank
+  private String name;
+
+  @ValidEnum(enumClass = Category.class)
+  private Category category;
+
+  @NotNull
+  private Long price;
+
+  @NotNull
+  private Long stock;
+
+  @NotBlank
+  private String description;
+
+  public ProductEntity toEntity(String productKey) {
+    return ProductEntity.builder()
+        .productKey(productKey)
+        .name(name)
+        .category(category)
+        .price(price)
+        .stock(stock)
+        .description(description)
+        .build();
+  }
+
+}
