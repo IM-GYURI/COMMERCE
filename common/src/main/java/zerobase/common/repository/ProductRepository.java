c271426e87a3e7e74abcae47da430f0b580fb208
IM-GYURI <kgulr0517@gmail.com>
2024-06-12 18:54:58 +0900

    feat : add findAllByNameContains

--- common/src/main/java/zerobase/common/repository/ProductRepository.java
+++ common/src/main/java/zerobase/common/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package zerobase.common.repository;
 
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import zerobase.common.entity.ProductEntity;
 
@@ -9,4 +11,6 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
   Optional<ProductEntity> findByProductKey(String productKey);
 
   void deleteByProductKey(String productKey);
+
+  Page<ProductEntity> findAllByNameContains(String name, Pageable pageable);
 }
