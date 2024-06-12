b6e685e750fa1111fc4dfbf0cb5d738ca7286f58
IM-GYURI <kgulr0517@gmail.com>
2024-06-13 03:36:12 +0900

    feat : add CartItemEntity

---
+++ customer-api/src/main/java/zerobase/customerapi/entity/CartItemEntity.java
@@ -0,0 +1,35 @@
+package zerobase.customerapi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Table(indexes = {@Index(columnList = "cart_id")})
+@Entity(name = "CartItem")
+public class CartItemEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "cart_id")
+  private CartEntity cart;
+
+  private String productKey;
+
+  private Long price;
+
+  private Long count;
+
+}
