eded5157d8905f37bd94a2546dc815f06d266678
IM-GYURI <kgulr0517@gmail.com>
2024-06-12 22:18:15 +0900

    feat : add findAllByCategory

--- common/src/main/java/zerobase/common/repository/ProductRepository.java
+++ common/src/main/java/zerobase/common/repository/ProductRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import zerobase.common.entity.ProductEntity;
+import zerobase.common.type.Category;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
@@ -13,4 +14,6 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
   void deleteByProductKey(String productKey);
 
   Page<ProductEntity> findAllByNameContains(String name, Pageable pageable);
+
+  Page<ProductEntity> findAllByCategory(Category category, Pageable pageable);
 }
