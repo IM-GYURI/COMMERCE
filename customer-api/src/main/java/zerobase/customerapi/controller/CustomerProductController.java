e1dff6c19e07acac3559bc59d37a7c0fb315957e
IM-GYURI <kgulr0517@gmail.com>
2024-06-12 18:31:29 +0900

    feat : add get product list sorted by name

--- customer-api/src/main/java/zerobase/customerapi/controller/CustomerProductController.java
+++ customer-api/src/main/java/zerobase/customerapi/controller/CustomerProductController.java
@@ -1,11 +1,13 @@
 package zerobase.customerapi.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import zerobase.common.dto.product.ProductListDto;
 import zerobase.customerapi.service.CustomerProductService;
 
 @RequestMapping("/product")
@@ -15,8 +17,26 @@ public class CustomerProductController {
 
   private final CustomerProductService customerProductService;
 
+  /**
+   * 상품 상세 조회
+   *
+   * @param productKey
+   * @return
+   */
   @GetMapping("/{productKey}")
   public ResponseEntity<?> productDetail(@PathVariable String productKey) {
     return ResponseEntity.ok(customerProductService.information(productKey));
+  }
+
+  /**
+   * 상품 전체 조회 : 가나다순
+   *
+   * @return
+   */
+  @GetMapping("/sorted-by-name")
+  public ResponseEntity<?> sortedByName() {
+    List<ProductListDto> productList = customerProductService.getAllSortedProductListByName();
+
+    return ResponseEntity.ok(productList);
   }
 }
--- customer-api/src/main/java/zerobase/customerapi/service/CustomerProductService.java
+++ customer-api/src/main/java/zerobase/customerapi/service/CustomerProductService.java
@@ -2,10 +2,14 @@ package zerobase.customerapi.service;
 
 import static zerobase.common.exception.CommonErrorCode.PRODUCT_NOT_FOUND;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import zerobase.common.dto.product.ProductDto;
+import zerobase.common.dto.product.ProductListDto;
 import zerobase.common.entity.ProductEntity;
 import zerobase.common.exception.CommonCustomException;
 import zerobase.common.repository.ProductRepository;
@@ -17,12 +21,39 @@ public class CustomerProductService {
 
   private final ProductRepository productRepository;
 
+  /**
+   * 상품 상세 정보 조회
+   *
+   * @param productKey
+   * @return
+   */
   public ProductDto information(String productKey) {
     return ProductDto.fromEntity(findByProductKeyOrThrow(productKey));
   }
 
+  /**
+   * 상품 키를 통해 상품이 존재하는지 확인
+   *
+   * @param productKey
+   * @return
+   */
   public ProductEntity findByProductKeyOrThrow(String productKey) {
     return productRepository.findByProductKey(productKey)
         .orElseThrow(() -> new CommonCustomException(PRODUCT_NOT_FOUND));
+  }
+
+  /**
+   * 상품 전체 조회 : 가나다순
+   *
+   * @return
+   */
+  public List<ProductListDto> getAllSortedProductListByName() {
+    List<ProductEntity> productList = productRepository.findAll();
+
+    return productList.stream()
+        .sorted(Comparator.comparing(ProductEntity::getName))
+        .map(productEntity -> new ProductListDto(productEntity.getName(),
+            productEntity.getCategory(), productEntity.getPrice(), productEntity.getStock()))
+        .collect(Collectors.toList());
   }
 }
