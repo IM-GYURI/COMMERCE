4c1651c3b40d5856028d17e83ae6635928f85199
IM-GYURI <kgulr0517@gmail.com>
2024-06-13 21:30:55 +0900

    feat : add Builder and updateCount

--- customer-api/src/main/java/zerobase/customerapi/entity/CartItemEntity.java
+++ customer-api/src/main/java/zerobase/customerapi/entity/CartItemEntity.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -32,4 +33,15 @@ public class CartItemEntity {
 
   private Long count;
 
+  @Builder
+  public CartItemEntity(CartEntity cartEntity, String productKey, Long price, Long count) {
+    this.cart = cartEntity;
+    this.productKey = productKey;
+    this.price = price;
+    this.count = count;
+  }
+
+  public void updateCount(Long count) {
+    this.count += count;
+  }
 }
