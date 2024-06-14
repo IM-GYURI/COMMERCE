8f945a38d298a381adf2063ec98615109e6945c0
IM-GYURI <kgulr0517@gmail.com>
2024-06-14 19:37:35 +0900

    feat : add dto for order item

---
+++ customer-api/src/main/java/zerobase/customerapi/dto/order/OrderItemDto.java
@@ -0,0 +1,29 @@
+package zerobase.customerapi.dto.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import zerobase.customerapi.entity.OrderItemEntity;
+
+/**
+ * 주문 아이템 dto
+ */
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemDto {
+
+  private String productKey;
+  private Long price;
+  private Long count;
+
+  public static OrderItemDto fromEntity(OrderItemEntity orderItemEntity) {
+    return OrderItemDto.builder()
+        .productKey(orderItemEntity.getProductKey())
+        .price(orderItemEntity.getPrice())
+        .count(orderItemEntity.getCount())
+        .build();
+  }
+}
